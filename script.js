3bd34cb54467eea6bb73385851caa2ddc62052a5
Edilson Sousa <edsouzasilv@hotmail.com>
2019-09-10 14:34:34 -0300

    Criando as funcionalidades que cria os balões na tela e define suas posições

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function addBalao() {
     var balao = document.createElement("div");
-    balao.setAttribute("class, balao");
-
+    balao.setAttribute("class", "balao");
     var p1 = Math.floor(Math.random() * 500);
+    var p2 = Math.floor(Math.random() * 400);
+    balao.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+    balao.setAttribute("onclick", "estourar(this)");
+    
+    document.body.appendChild(balao);
+}
+function estourar(elemento) {
+    document.body.removeChild(elemento);
 }
