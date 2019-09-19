d85d1558c3fe55e09f1240891ab672c888e29fba
Edilson Sousa <edsouzasilv@hotmail.com>
2019-09-18 21:58:30 -0300

    Aumentando quantidade de balões na horizontal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function addBalao() {
     var balao = document.createElement("div");
     balao.setAttribute("class", "balao");
-    var p1 = Math.floor(Math.random() * 500);
+    var p1 = Math.floor(Math.random() * 600);
     var p2 = Math.floor(Math.random() * 400);
     balao.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
     balao.setAttribute("onclick", "estourar(this)");
